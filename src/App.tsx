ef8c59252f578a087808d5c5f147bce66e369eff
Hítalo Rodrigues <hitalo005@gmail.com>
2023-03-15 23:30:39 -0300

    calculos funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -8,8 +9,10 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
       
-      <Transactions />
     </ThemeProvider>   
 
   )
